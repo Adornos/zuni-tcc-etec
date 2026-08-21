0776f289055c2879f1a24ace80abe3c3b4ab0bec
joaovitorbardev <joaovitorbarbr@gmail.com>
2026-08-21 13:56:18 -0300

    zuni-cordinator-dashboard-relatórios

--- zuni-web/resources/views/coordinator/enrollments.blade.php
+++ zuni-web/resources/views/coordinator/enrollments.blade.php
@@ -1,4 +1,6 @@
 <!-- row-span-4 col-span-4 card 100% -->
+ <!-- cada card deve ser clicável e quando for clicado, deve redirecionar para a página de matrículas da respectiva sala -->
+  <!-- cada ano tem a turma A ou B ou C ou D, na qual, são turmas com alunos diferentes, vc muda clicando na setinha que deve estar ao lado do ano com a série, assim: 3°ANO C ->  -->
 <div class="card bg-base-100 shadow-md 
 row-span-4 col-span-4 flex-col p-[1vmax] bg-Ccard overflow-y-auto">
     <div class="card-body flex flex-col">
@@ -31,7 +33,8 @@ row-span-4 col-span-4 flex-col p-[1vmax] bg-Ccard overflow-y-auto">
 
             <!-- Lista -->
             <div class="mt-16 space-y-10">
-
+        
+        @foreach(range(1, 6) as $alunos)
                 <!-- Aluno -->
                 <div
                     class="flex h-[84px] items-center rounded-[25px] bg-[#f8fbff]
@@ -46,81 +49,6 @@ row-span-4 col-span-4 flex-col p-[1vmax] bg-Ccard overflow-y-auto">
                         Aderildo da Silva Oliveira
                     </span>
                 </div>
-                                <!-- Aluno -->
-                <div
-                    class="flex h-[84px] items-center rounded-[25px] bg-[#f8fbff]
-                           px-8 shadow-[0_5px_12px_rgba(90,110,140,0.10)]
-                           transition hover:-translate-y-0.5 hover:shadow-md"
-                >
-                
-                <!-- Implementar a imagem colocada na matrícula do aluno, caso não tenha imagem, colocar a imagem padrão do usuário. -->
-                <div class="mr-7 h-[46px] w-[46px] rounded-full bg-[#d9d9d9]"></div>
-
-                    <span class="text-[19px] font-medium text-[#222]">
-                        Aderildo da Silva Oliveira
-                    </span>
-                </div>
-                                <!-- Aluno -->
-                <div
-                    class="flex h-[84px] items-center rounded-[25px] bg-[#f8fbff]
-                           px-8 shadow-[0_5px_12px_rgba(90,110,140,0.10)]
-                           transition hover:-translate-y-0.5 hover:shadow-md"
-                >
-                
-                <!-- Implementar a imagem colocada na matrícula do aluno, caso não tenha imagem, colocar a imagem padrão do usuário. -->
-                <div class="mr-7 h-[46px] w-[46px] rounded-full bg-[#d9d9d9]"></div>
-
-                    <span class="text-[19px] font-medium text-[#222]">
-                        Aderildo da Silva Oliveira
-                    </span>
-                </div>
-                
-                                                <!-- Aluno -->
-                <div
-                    class="flex h-[84px] items-center rounded-[25px] bg-[#f8fbff]
-                           px-8 shadow-[0_5px_12px_rgba(90,110,140,0.10)]
-                           transition hover:-translate-y-0.5 hover:shadow-md"
-                >
-                
-                <!-- Implementar a imagem colocada na matrícula do aluno, caso não tenha imagem, colocar a imagem padrão do usuário. -->
-                <div class="mr-7 h-[46px] w-[46px] rounded-full bg-[#d9d9d9]"></div>
-
-                    <span class="text-[19px] font-medium text-[#222]">
-                        Aderildo da Silva Oliveira
-                    </span>
-                </div>
-                                                <!-- Aluno -->
-                <div
-                    class="flex h-[84px] items-center rounded-[25px] bg-[#f8fbff]
-                           px-8 shadow-[0_5px_12px_rgba(90,110,140,0.10)]
-                           transition hover:-translate-y-0.5 hover:shadow-md"
-                >
-                
-                <!-- Implementar a imagem colocada na matrícula do aluno, caso não tenha imagem, colocar a imagem padrão do usuário. -->
-                <div class="mr-7 h-[46px] w-[46px] rounded-full bg-[#d9d9d9]"></div>
-
-                    <span class="text-[19px] font-medium text-[#222]">
-                        Aderildo da Silva Oliveira
-                    </span>
-                </div>
-                                                <!-- Aluno -->
-                <div
-                    class="flex h-[84px] items-center rounded-[25px] bg-[#f8fbff]
-                           px-8 shadow-[0_5px_12px_rgba(90,110,140,0.10)]
-                           transition hover:-translate-y-0.5 hover:shadow-md"
-                >
-                
-                <!-- Implementar a imagem colocada na matrícula do aluno, caso não tenha imagem, colocar a imagem padrão do usuário. -->
-                <div class="mr-7 h-[46px] w-[46px] rounded-full bg-[#d9d9d9]"></div>
-
-                    <span class="text-[19px] font-medium text-[#222]">
-                        Aderildo da Silva Oliveira
-                    </span>
-                </div>
+        @endforeach
+            </div>
     </div>
-<div class="container">
-
-    <h1>Enrollments</h1>
-    {{print_r($enrollments);}}
-
-</div>
